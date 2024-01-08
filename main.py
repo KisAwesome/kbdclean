9f5532a9f4c0102b421ed0ec1d12a0fe7bed5885
KisAwesome <cool676rock@gmail.com>
2024-01-08 16:40:07 +0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+import argparse
+import time
+import pynput.keyboard as keyboard
+
+
+
+def suppress_keypress(max_time):
+    with keyboard.Listener( suppress=True) as listener:
+        if max_time is None:
+            listener.join()
+        else:
+            time.sleep(max_time)
+            listener.stop()
+
+def main():
+    parser = argparse.ArgumentParser(description='CLI app to suppress key presses for a specified duration.')
+    parser.add_argument('--max-time', type=float, default=None, help='Maximum time to suppress key presses (in seconds)')
+
+    args = parser.parse_args()
+    try:
+        suppress_keypress(args.max_time)
+    except KeyboardInterrupt:
+        pass
+
+if __name__ == "__main__":
+    main()
