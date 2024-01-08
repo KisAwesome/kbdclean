c80f1fbb710fda9b49c3f9a25d66495df8895596
KisAwesome <cool676rock@gmail.com>
2024-01-08 16:42:24 +0400

    Added message when suppressing indefinitely

--- main.py
+++ main.py
@@ -4,24 +4,34 @@ import time
 import pynput.keyboard as keyboard
 
 
-
 def suppress_keypress(max_time):
-    with keyboard.Listener( suppress=True) as listener:
+    with keyboard.Listener(suppress=True) as listener:
         if max_time is None:
             listener.join()
         else:
             time.sleep(max_time)
             listener.stop()
 
+
 def main():
-    parser = argparse.ArgumentParser(description='CLI app to suppress key presses for a specified duration.')
-    parser.add_argument('--max-time', type=float, default=None, help='Maximum time to suppress key presses (in seconds)')
+    parser = argparse.ArgumentParser(
+        description="CLI app to suppress key presses for a specified duration."
+    )
+    parser.add_argument(
+        "--max-time",
+        type=float,
+        default=None,
+        help="Maximum time to suppress key presses (in seconds)",
+    )
 
     args = parser.parse_args()
+    if args.max_time is None:
+        print("Suppressing key presses indefinitely press cmd+. to interrupt")
     try:
         suppress_keypress(args.max_time)
     except KeyboardInterrupt:
         pass
+
 
 if __name__ == "__main__":
     main()
